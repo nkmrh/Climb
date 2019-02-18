0215210d3521c31d7f3d35385f09919b09278966
nkmrh <hajimeet@me.com>
2019-02-18 21:47:11 +0900

    Add Mountain

---
+++ Climb/Mountain.swift
@@ -0,0 +1,40 @@
+//
+//  Mountain.swift
+//  Climb
+//
+//  Created by Hajime Nakamura on 2019/02/18.
+//  Copyright © 2019 Hajime Nakamura. All rights reserved.
+//
+
+import Foundation
+
+enum DifficultyLevel: Int, Codable {
+    case low = 1, middle, high
+}
+
+enum PhysicalLevel: Int, Codable {
+    case low = 1, middle, high
+}
+
+struct Mountain: Equatable, Codable {
+    let id: Int
+    let name: String
+    let description: String
+    let thumbnailUrl: URL
+    let imageUrl: URL
+    let elevation: Int
+    let latitude: Double
+    let longitude: Double
+    let prefectures: [String]
+    let areaId: Int
+    let likeCount: Int
+    let isLike: Bool
+    let activityCount: Int
+    let viewCount: Int
+    let difficultyLevel: DifficultyLevel
+    let physicalLevel: PhysicalLevel
+
+    static func == (lhs: Mountain, rhs: Mountain) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
